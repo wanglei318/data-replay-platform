2fecc77ce06711fabb085ce56f642b5280c57f5d
wanglei318 <15851861998@163.com>
2025-03-28 15:37:26 +0800

    添加 Vue 主应用组件

---
+++ frontend/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <el-container>
+    <el-header>
+      <h1>数据回放平台</h1>
+    </el-header>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.el-header {
+  background-color: #409EFF;
+  color: white;
+  text-align: center;
+  line-height: 60px;
+}
+
+.el-header h1 {
+  margin: 0;
+}
+
+.el-main {
+  padding: 20px;
+}
+</style>
